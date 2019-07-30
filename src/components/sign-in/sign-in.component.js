fb605a6ffa13a33166027c749879959d2c051c84
Brian MacPherson <brimac1634@gmail.com>
2019-07-30 13:24:09 +0800

    fixed google auth, added sign in and signout options

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -46,12 +46,15 @@ class SignIn extends Component {
 						handleChange={this.handleChange}
 						required 
 					/>
-					<CustomButton type='submit'> Sign In </CustomButton>
-					<CustomButton 
-						onClick={signInWithGoogle}
-					> 
-						Sign In With Google
-					</CustomButton>
+					<div className='buttons'>
+						<CustomButton type='submit'> Sign In </CustomButton>
+						<CustomButton 
+							isGoogleSignIn={true}
+							onClick={signInWithGoogle}
+						> 
+							Sign In With Google
+						</CustomButton>
+					</div>
 				</form>
 			</div>
 		)
