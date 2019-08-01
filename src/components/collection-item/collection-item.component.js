d38ed9b1b7c6a5e3cb65f0036fbf7adc69b40fd3
Brian MacPherson <brimac1634@gmail.com>
2019-08-01 12:10:41 +0800

    adjusted styling of custom button and collection item, added favicon

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
 
@@ -8,8 +9,9 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
 			<div className='image' style={{backgroundImage: `url(${imageUrl}`}}/>
 			<div className='collection-footer'>
 				<span className='name'>{ name }</span>
-				<span className='price'>{ price }</span>
+				<span className='price'>{`HKD$${price}`}</span>
 			</div>
+			<CustomButton inverted> Add to cart </CustomButton>
 		</div>
 	)
 }
