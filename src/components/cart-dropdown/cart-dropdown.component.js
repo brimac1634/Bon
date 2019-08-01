3e1a3b7f8a50a15c7832e5405d6439ac77abf981
Brian MacPherson <brimac1634@gmail.com>
2019-08-01 16:43:28 +0800

    added quantity to cartItems reducer and added cart items component to appear in shopping cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import './cart-dropdown.styles.scss';
-import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux';
 
-const CartDropdown = () => (
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+import './cart-dropdown.styles.scss';
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+	cartItems
+})
+
+const CartDropdown = ({ cartItems }) => (
 	<div className='cart-dropdown'>
-		<div className='cart-items'/>
+		<div className='cart-items'>
+			{
+				cartItems.map(item => <CartItem key={item.id} item={item} />)
+			}
+		</div>
 		<CustomButton>GO TO CHECKOUT</CustomButton>
 	</div>
 )
 
-export default CartDropdown;
+export default connect(mapStateToProps)(CartDropdown);
