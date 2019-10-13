fd84a64170f417bc7de031b6b9018e60e42cd93e
Brian MacPherson <brimac1634@gmail.com>
2019-10-13 20:54:55 +0800

    moved client to separate project folder

--- client/src/pages/product/product.component.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import ProductDetails from '../../components/product-details/product-details.component';
-
-const Product = () => <ProductDetails />
-
-export default Product;
--- client/src/redux/alert/alert.types.js
+++
@@ -1,6 +0,0 @@
-const AlertActionTypes = {
-	SET_ALERT_MESSAGE: 'SET_ALERT_MESSAGE',
-	TOGGLE_ALERT: 'TOGGLE_ALERT'
-}
-
-export default AlertActionTypes;
